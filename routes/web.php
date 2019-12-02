2cd505d03588601b4771bc5cbfb2530ee0a8d889
vagrant <vagrant@homestead>
2019-12-02 19:50:35 +0000

    Se utilizo eloquent

--- routes/web.php
+++ routes/web.php
@@ -23,33 +23,45 @@ Route::get('/contacto', 'MiControlador@contactar');
 Route::get('/galeria', 'MiControlador@galeria');
 
 
-Route::get('/insertar', function(){
+// Route::get('/insertar', function(){
 
-    DB::insert("INSERT INTO articulos (NOMBRE_ARTICULO, PRECIO, PAIS_ORIGEN, SECCION, OBSERVACIONES) VALUES(?,?,?,?,?)",
-    ["JARRON", 15.2, "JAPON", "CERAMICA", "GANGA"]);
+//     DB::insert("INSERT INTO articulos (NOMBRE_ARTICULO, PRECIO, PAIS_ORIGEN, SECCION, OBSERVACIONES) VALUES(?,?,?,?,?)",
+//     ["JARRON", 15.2, "JAPON", "CERAMICA", "GANGA"]);
 
-});
+// });
 
-Route::get('/leer', function(){
+// Route::get('/leer', function(){
 
-    $resultados = DB::select("SELECT * FROM articulos WHERE ID=?", [1]);
+//     $resultados = DB::select("SELECT * FROM articulos WHERE ID=?", [1]);
 
-    foreach ($resultados as $articulo) {
+//     foreach ($resultados as $articulo) {
         
-        return $articulo-> Nombre_Articulo;
+//         return $articulo-> Nombre_Articulo;
+
+//     }
+
+// });
+
+// Route::get('/actualiza', function(){
+
+//     DB::update("UPDATE articulos SET SECCION='DECORACION' WHERE ID=?", [1]);
+
+// });
+
+// Route::get('/borrar', function(){
+
+//     DB::update("DELETE FROM articulos WHERE ID=?", [1]);
+
+// });
+
+Route::get("/leer", function(){
+
+    $articulos=App\Articulo::all();
+
+    foreach ($articulos as $articulo) {
+        
+        echo "Nombre: " . $articulo->Nombre_Articulo . " Precio: " .$articulo->Precio . "<br>";
 
     }
-
-});
-
-Route::get('/actualiza', function(){
-
-    DB::update("UPDATE articulos SET SECCION='DECORACION' WHERE ID=?", [1]);
-
-});
-
-Route::get('/borrar', function(){
-
-    DB::update("DELETE FROM articulos WHERE ID=?", [1]);
 
 });
