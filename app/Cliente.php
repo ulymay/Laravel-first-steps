08821ae7df853182933b2086a4aa4b9755250668
ulymay <ulymay@gmail.com>
2019-12-05 20:20:30 +0000

    archivos faltantes

---
+++ app/Cliente.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    //
+
+    public function articulo(){
+
+        return $this->hasOne('App\Articulo');
+
+    }
+
+    public function articulos()
+    {
+        return $this->hasMany('App\Articulo');
+    }
+}
